a0402dfc47287ecab24aa1fbde12eca48a207c66
Jakub Mandra <mandra.jakub@gmail.com>
2020-01-12 17:09:29 +0100

    Add fixed kanban data

---
+++ src/utils/data.ts
@@ -0,0 +1,56 @@
+const columns = [
+    {
+        id: "col1",
+        title: "Backlog",
+        items: [{
+            id: "item1",
+            title: "Item one"
+        },
+        {
+            id: "item2",
+            title: "Item two"
+        },
+        {
+            id: "item3",
+            title: "Item three"
+        },]
+    },
+    {
+        id: "col2",
+        title: "In progress",
+        items: [{
+            id: "item4",
+            title: "Item four"
+        },
+        {
+            id: "item5",
+            title: "Item five"
+        },
+        {
+            id: "item6",
+            title: "Item six"
+        },]
+    },
+    {
+        id: "col3",
+        title: "Testing",
+        items: [{
+            id: "item7",
+            title: "Item seven"
+        },
+        {
+            id: "item8",
+            title: "Item eight"
+        }]
+    },
+    {
+        id: "col4",
+        title: "Done",
+        items: [{
+            id: "item9",
+            title: "Item nine"
+        },]
+    }
+]
+
+export { columns };
