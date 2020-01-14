0b9dcaf12f511d2100459c69be5cdc48672e52a9
Jakub Mandra <mandra.jakub@gmail.com>
2020-01-14 21:13:02 +0100

    Connect with firebase (do not push config)

--- src/components/kanbanBoard/KanbanBoardContainer.tsx
+++ src/components/kanbanBoard/KanbanBoardContainer.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
+import { db } from "../../utils/firebase";
 import { tasksFetchSuccess } from "../../redux/actions";
 import { columns as data } from "../../utils/data";
 import KanbanBoardView from "./KanbanBoardView";
@@ -10,7 +11,34 @@ const KanbanBoardContainer: React.FC = () => {
     const dispatch = useDispatch();
 
     const fetchColumns = useCallback(() => {
-        dispatch(tasksFetchSuccess(data));
+        let cols = [];
+        db.collection("columns")
+            .get()
+            .then(qSnap =>
+                qSnap.forEach(snap => {
+                    console.log("SNAP ID", snap.id);
+                    cols.push({ id: snap.id, ...snap.data() });
+                }),
+            )
+            .then(s => dispatch(tasksFetchSuccess(cols)));
+
+            console.log("COLS", cols)
+        // db.collection(`columns`).get().then(snap => {
+        //     snap.forEach(childSnap => {
+        //         const val = childSnap.data();
+        //         const id = childSnap.id;
+
+        //         console.log("KEY", id, "VAL", val)
+        //     })
+
+        // console.log(cols.tasks)
+        // const colVals = cols.forEach()
+        // console.log(vals)
+        // dispatch(tasksFetchSuccess());
+        // });
+        // db.ref("/columns/-LyVVXlmHTHJPH41ud_p/tasks").push({
+        //     title: "Item seven"
+        // })
     }, []);
 
     useEffect(() => {
