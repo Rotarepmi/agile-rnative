09106c02b8756405ac3eda280b68872220ac5588
Jakub Mandra <mandra.jakub@gmail.com>
2020-01-20 21:35:59 +0100

    Update getCols

--- src/components/kanbanBoard/KanbanBoardContainer.tsx
+++ src/components/kanbanBoard/KanbanBoardContainer.tsx
@@ -11,34 +11,18 @@ const KanbanBoardContainer: React.FC = () => {
     const dispatch = useDispatch();
 
     const fetchColumns = useCallback(() => {
-        let cols = [];
         db.collection("columns")
+            .orderBy("place")
             .get()
-            .then(qSnap =>
+            .then(qSnap => {
+                let cols = [];
+
                 qSnap.forEach(snap => {
-                    console.log("SNAP ID", snap.id);
                     cols.push({ id: snap.id, ...snap.data() });
-                }),
-            )
-            .then(s => dispatch(tasksFetchSuccess(cols)));
-
-            console.log("COLS", cols)
-        // db.collection(`columns`).get().then(snap => {
-        //     snap.forEach(childSnap => {
-        //         const val = childSnap.data();
-        //         const id = childSnap.id;
-
-        //         console.log("KEY", id, "VAL", val)
-        //     })
-
-        // console.log(cols.tasks)
-        // const colVals = cols.forEach()
-        // console.log(vals)
-        // dispatch(tasksFetchSuccess());
-        // });
-        // db.ref("/columns/-LyVVXlmHTHJPH41ud_p/tasks").push({
-        //     title: "Item seven"
-        // })
+                });
+                return cols;
+            })
+            .then(cols => dispatch(tasksFetchSuccess(cols)));
     }, []);
 
     useEffect(() => {
