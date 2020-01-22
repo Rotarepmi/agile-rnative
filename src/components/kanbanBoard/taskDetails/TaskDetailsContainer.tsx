af97db63827c27b8442668cc15a12b851362c33b
Jakub Mandra <mandra.jakub@gmail.com>
2020-01-22 21:39:47 +0100

    Update task functions:
    - getTask
    - saveTask (on input blur)
    - change taskColumn

--- src/components/kanbanBoard/taskDetails/TaskDetailsContainer.tsx
+++ src/components/kanbanBoard/taskDetails/TaskDetailsContainer.tsx
@@ -1,19 +1,25 @@
 import React, { useCallback, useState, useEffect } from "react";
+import { ToastAndroid } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
 
 import TaskDetailsView from "./TaskDetailsView";
 
 import { db } from "../../../utils/firebase";
+import { Column } from "../../../utils/Types";
 
 interface Props {
     data: {
         id: string;
         title: string;
     };
+    columnId: Column["id"];
     detailsVisible: boolean;
     setDetailsVisible: () => void;
 }
 
-const TaskDetailsContainer: React.FC<Props> = ({ data, detailsVisible, setDetailsVisible }) => {
+const TaskDetailsContainer: React.FC<Props> = ({ data, detailsVisible, columnId, setDetailsVisible }) => {
+    const columns = useSelector(state => state.tasks.columns);
+
     const [task, setTask] = useState();
     const [description, setDescription] = useState("");
     const [title, setTitle] = useState("");
@@ -23,24 +29,75 @@ const TaskDetailsContainer: React.FC<Props> = ({ data, detailsVisible, setDetail
             .doc(id)
             .get()
             .then(qSnap => {
-                setTask(qSnap.data());
-                setDescription(qSnap.data().description);
-                setTitle(qSnap.data().title);
+                const data = qSnap.data();
+
+                setTask(data);
+                setDescription(data.description);
+                setTitle(data.title);
             })
-            .catch(e => console.log(e));
+            .catch(e => ToastAndroid.show("Error fetching task", ToastAndroid.SHORT));
     }, []);
 
     useEffect(() => {
-        getTask(data.id);
-    }, [getTask]);
+        if (detailsVisible) {
+            getTask(data.id);
+        }
+    }, [detailsVisible, getTask]);
 
     function handleSave() {
         db.collection("tasks")
             .doc(data.id)
             .update({
+                title,
                 description,
+                modifyDate: Date.now(),
             })
-            .catch(e => console.log(e));
+            .then(() => ToastAndroid.show("Changes saved...", ToastAndroid.SHORT))
+            .catch(e => ToastAndroid.show("Error saving description", ToastAndroid.SHORT));
+    }
+
+    // db.runTransaction(function(transaction) {
+    //     return transaction.get(sfDocRef).then(function(sfDoc) {
+    //         if (!sfDoc.exists) {
+    //             throw "Document does not exist!";
+    //         }
+
+    //         var newPopulation = sfDoc.data().population + 1;
+    //         if (newPopulation <= 1000000) {
+    //             transaction.update(sfDocRef, { population: newPopulation });
+    //             return newPopulation;
+    //         } else {
+    //             return Promise.reject("Sorry! Population is too big.");
+    //         }
+    //     });
+    // })
+
+    function handleColumnChange(newColumnId: Column["id"]) {
+        const sfDocRefOld = db.collection("columns").doc(columnId);
+        const sfDocRefNew = db.collection("columns").doc(newColumnId);
+
+        db.runTransaction(transaction =>
+            transaction.get(sfDocRefNew).then(sfDoc => {
+                if (!sfDoc.exists) {
+                    ToastAndroid.show("Column does not exist..", ToastAndroid.SHORT);
+                }
+
+                const newTasks = sfDoc.data().tasks;
+                newTasks.unshift(data);
+                transaction.update(sfDocRefNew, { tasks: newTasks });
+            }),
+        );
+
+        db.runTransaction(transaction =>
+            transaction.get(sfDocRefOld).then(sfDoc => {
+                if (!sfDoc.exists) {
+                    ToastAndroid.show("Column does not exist..", ToastAndroid.SHORT);
+                }
+
+                const newTasks = sfDoc.data().tasks.filter(t => t.id !== data.id);
+                transaction.update(sfDocRefOld, { tasks: newTasks });
+            }),
+        );
     }
 
     return (
@@ -49,6 +106,9 @@ const TaskDetailsContainer: React.FC<Props> = ({ data, detailsVisible, setDetail
             description={description}
             setDescription={setDescription}
             title={title}
+            columns={columns}
+            columnId={columnId}
+            handleColumnChange={handleColumnChange}
             setTitle={setTitle}
             detailsVisible={detailsVisible}
             handleSave={handleSave}
--- src/components/kanbanBoard/taskDetails/TaskDetailsView.tsx
+++ src/components/kanbanBoard/taskDetails/TaskDetailsView.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Modal, View, Text, TouchableHighlight, TextInput, StyleSheet } from "react-native";
+import { Modal, View, Text, TouchableHighlight, TextInput, StyleSheet, Picker } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
+import { formatDistanceToNow } from "date-fns";
+import { Column } from "../../../utils/Types";
 
 interface Props {
     task:
@@ -15,38 +17,66 @@ interface Props {
     description: string;
     title: string;
     detailsVisible: boolean;
+    columns: Column[];
+    columnId: Column["id"];
+    handleColumnChange: (value: Column["id"]) => void;
     setDescription: (value: string) => void;
     setTitle: (value: string) => void;
     handleSave: () => void;
     setDetailsVisible: () => void;
 }
 
-const TaskDetailsView: React.FC<Props> = ({ task, detailsVisible, title, description, setDescription, setTitle, handleSave, setDetailsVisible }) => {
+const TaskDetailsView: React.FC<Props> = ({
+    task,
+    detailsVisible,
+    title,
+    description,
+    columns,
+    columnId,
+    handleColumnChange,
+    setDescription,
+    setTitle,
+    handleSave,
+    setDetailsVisible,
+}) => {
     if (!task) return null;
     return (
         <Modal animationType="slide" transparent={false} visible={detailsVisible}>
             <View>
                 <View style={styles.header}>
-                    <TouchableHighlight onPress={setDetailsVisible}>
-                        <FontAwesome name="chevron-left" size={15} />
+                    <TouchableHighlight style={styles.closeBtn} onPress={setDetailsVisible}>
+                        <View style={styles.closeBtn}>
+                            <FontAwesome name="chevron-left" size={15} />
+                            <Text style={styles.closeBtnTxt} numberOfLines={1} ellipsizeMode="tail">
+                                {title}
+                            </Text>
+                        </View>
                     </TouchableHighlight>
-                    <Text style={{ marginLeft: 5 }}>{title}</Text>
                 </View>
 
                 <View style={styles.contentWrapper}>
-                    <TextInput style={styles.title} multiline value={title} onChangeText={(value: string) => setTitle(value)} />
-                    <Text>Created: {`${new Date(task.createDate)}`}</Text>
+                    <TextInput
+                        style={styles.title}
+                        multiline
+                        value={title}
+                        onChangeText={(value: string) => setTitle(value)}
+                        onEndEditing={handleSave}
+                    />
+                    <Text>Created: {formatDistanceToNow(Number(task.createDate))} ago</Text>
+                    {task.modifyDate && <Text>Modified: {formatDistanceToNow(Number(task.modifyDate))} ago</Text>}
+                    <Picker selectedValue={columnId} onValueChange={value => handleColumnChange(value)}>
+                        {columns.map(c => (
+                            <Picker.Item label={c.title} value={c.id} key={c.id} />
+                        ))}
+                    </Picker>
                     <TextInput
                         style={styles.description}
                         multiline
                         value={description}
                         placeholder="Start writing your ideas here..."
                         onChangeText={(value: string) => setDescription(value)}
+                        onEndEditing={handleSave}
                     />
-
-                    <TouchableHighlight onPress={handleSave}>
-                        <Text>Save</Text>
-                    </TouchableHighlight>
                 </View>
             </View>
         </Modal>
@@ -55,11 +85,22 @@ const TaskDetailsView: React.FC<Props> = ({ task, detailsVisible, title, descrip
 
 const styles = StyleSheet.create({
     header: {
-        padding: 10,
         borderBottomWidth: 1,
         borderBottomColor: "#999",
         flexDirection: "row",
         alignItems: "center",
+    },
+    closeBtn: {
+        // maxWidth: "30%",
+        height: "100%",
+        padding: 5,
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    closeBtnTxt: {
+        width: 100,
+        marginLeft: 5,
+        overflow: "hidden",
     },
     contentWrapper: {
         padding: 25,
@@ -70,7 +111,7 @@ const styles = StyleSheet.create({
     },
     description: {
         paddingTop: 20,
-        paddingBottom: 20
+        paddingBottom: 20,
     },
 });
 
