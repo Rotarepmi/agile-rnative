69780fbaa5e3b3681334e7a9d04774b746889476
Jakub Mandra <mandra.jakub@gmail.com>
2020-01-23 21:37:20 +0100

    Add loading Create signUp signIn loading components

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+
+interface Props {
+    navigation: any;
+}
+
+const SignIn: React.FC<Props> = ({ navigation }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+    function handleLogin() {
+        // TODO: Firebase stuff...
+        console.log("handleLogin");
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Login</Text>
+            {errorMessage && <Text style={{ color: "red" }}>{errorMessage}</Text>}
+            <TextInput
+                style={styles.textInput}
+                autoCapitalize="none"
+                placeholder="Email"
+                onChangeText={email => setEmail(email)}
+                value={email}
+            />
+            <TextInput
+                secureTextEntry
+                style={styles.textInput}
+                autoCapitalize="none"
+                placeholder="Password"
+                onChangeText={password => setPassword(password)}
+                value={password}
+            />
+            <Button title="Login" onPress={handleLogin} />
+            <Button title="Don't have an account? Sign Up" onPress={() => navigation.navigate("SignUp")} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    textInput: {
+        height: 40,
+        width: "90%",
+        borderColor: "gray",
+        borderWidth: 1,
+        marginTop: 8,
+    },
+});
+
+export default SignIn;
