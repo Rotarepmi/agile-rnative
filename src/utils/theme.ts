045e74338361c21e957882e358bb4016015bd347
Jakub Mandra <mandra.jakub@gmail.com>
2020-02-15 22:01:20 +0100

    Add theme

---
+++ src/utils/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+    white: "#fff",
+    primary: "#2196f3",
+    primaryLight: "#6ec6ff",
+    primaryDark: "#0069c0",
+    secondary: "#26c6da",
+    secondaryLight: "#6ff9ff",
+    secondaryDark: "#0095a8"
+}
