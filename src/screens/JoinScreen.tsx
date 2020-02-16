324b0347f032683ccb1f6ba7d58b2a2a9d947e9f
Jakub Mandra <mandra.jakub@gmail.com>
2020-02-16 17:10:30 +0100

    Update project joining flow

--- src/screens/JoinScreen.tsx
+++ src/screens/JoinScreen.tsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, Vibration, Dimensions, Alert, ToastAndroid } from "react-native";
+import { Text, View, StyleSheet, Vibration, Dimensions, Alert, ToastAndroid, ActivityIndicator } from "react-native";
 import { Camera } from "expo-camera";
 import * as Permissions from "expo-permissions";
 import { NavigationSwitchProp } from "react-navigation";
+import { useDispatch } from "react-redux";
+
+import { setActiveProject } from "../redux/actions";
 import firebase, { db } from "../utils/firebase";
 
 interface Props {
@@ -13,9 +16,11 @@ const { width } = Dimensions.get("window");
 const qrSize = width;
 
 const JoinScreen: React.FC<Props> = ({ navigation }) => {
+    const dispatch = useDispatch();
 
     const [hasCameraPermission, setHasCameraPermission] = useState(null);
     const [scanned, setScanned] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         async function init() {
@@ -30,63 +35,62 @@ const JoinScreen: React.FC<Props> = ({ navigation }) => {
         Vibration.vibrate(500);
         setScanned(true);
 
-        const docRef = db.collection("projects").doc(data);
-        docRef.get().then(doc => {
-            if (doc.exists) {
-                const projData = doc.data();
+        if (data.startsWith("joinToProject:")) {
+            const projKey = data.substring(14);
+            const docRef = db.collection("projects").doc(projKey);
 
-                Alert.alert("Join to project", `Do you want to join "${projData.name}" project?`, [
-                    {
-                        text: "Cancel",
-                        onPress: () => setScanned(false),
-                        style: "cancel",
-                    },
-                    { text: "Confirm", onPress: () => joinToProject(docRef) },
-                ]);
-            }
-        });
+            docRef.get().then(doc => {
+                if (doc.exists) {
+                    const projData = doc.data();
+
+                    Alert.alert("Join to project", `Do you want to join "${projData.name}" project?`, [
+                        {
+                            text: "Cancel",
+                            onPress: () => setScanned(false),
+                            style: "cancel",
+                        },
+                        { text: "Confirm", onPress: () => joinToProject(docRef, projData.name, docRef.id) },
+                    ]);
+                }
+            });
+        } else {
+            Alert.alert("Join to project", "That data has no sense.");
+        }
     }
 
-    function joinToProject(projDocRef: firebase.firestore.DocumentReference<firebase.firestore.DocumentData>) {
+    function joinToProject(projDocRef: firebase.firestore.DocumentReference<firebase.firestore.DocumentData>, projName: string, projId: string) {
+        setLoading(true);
+
         const currentUser = firebase.auth().currentUser;
         const userDocRef = db.collection("users").doc(currentUser.uid);
 
-        let projName = "";
-
-        db.runTransaction(transaction => {
-            return transaction.get(projDocRef).then(projDoc => {
-                if (!projDoc.exists) {
-                    throw "Join failed - project not found";
-                    // ToastAndroid.show("Join failed", ToastAndroid.SHORT);
-                }
-
-                projName = projDoc.data().name;
-
-                const newProjUsers = projDoc.data().users;
-                newProjUsers.push({
+        const joinToProj = projDocRef.set(
+            {
+                users: firebase.firestore.FieldValue.arrayUnion({
                     id: currentUser.uid,
-                    name: currentUser.displayName
-                });
+                    name: currentUser.displayName,
+                }),
+            },
+            { merge: true },
+        );
 
-                transaction.update(projDocRef, { users: newProjUsers });
-            });
-        });
+        const joinToUsers = userDocRef.set(
+            {
+                projects: firebase.firestore.FieldValue.arrayUnion({
+                    id: projId,
+                    name: projName,
+                }),
+            },
+            { merge: true },
+        );
 
-        db.runTransaction(transaction => {
-            return transaction.get(userDocRef).then(userDoc => {
-                if (!userDoc.exists) {
-                    throw "Join failed - user not found";
-                }
-
-                const newUserProjects = userDoc.data().projects;
-                newUserProjects.push({
-                    id: projDocRef.id,
-                    name: projName
-                });
-
-                transaction.update(userDocRef, { projects: newUserProjects });
-            });
-        });
+        Promise.all([joinToProj, joinToUsers])
+            .then(() => {
+                setLoading(false);
+                dispatch(setActiveProject(projDocRef.id));
+                navigation.navigate("Tasks", { projectId: projId, routeName: projName });
+            })
+            .catch(e => ToastAndroid.show(e.message, ToastAndroid.SHORT));
     }
 
     if (hasCameraPermission === false) {
@@ -98,6 +102,9 @@ const JoinScreen: React.FC<Props> = ({ navigation }) => {
             <Camera onBarCodeScanned={scanned ? undefined : handleBarCodeScanned} style={[StyleSheet.absoluteFill, styles.qrElement]}>
                 <Text style={styles.description}>Scan the code</Text>
                 {/* <Image style={styles.qr} source={require("../assets/qr.png")} /> */}
+                {
+                    loading && <ActivityIndicator size="large" />
+                }
                 <Text onPress={() => (scanned ? setScanned(false) : navigation.goBack())} style={styles.cancel}>
                     {!scanned ? "Cancel" : "Try again"}
                 </Text>
--- src/screens/ShareScreen.tsx
+++ src/screens/ShareScreen.tsx
@@ -3,16 +3,15 @@ import { useSelector } from "react-redux";
 import { View, Text, StyleSheet, Dimensions } from "react-native";
 import QRCode from "react-native-qrcode";
 
-
 const ShareScreen: React.FC = () => {
     const activeProject = useSelector(state => state.projects.activeProject);
     const dim = Dimensions.get("screen");
 
-    if(dim) {
+    if (dim) {
         return (
             <View style={styles.qrWrapper}>
                 <Text style={styles.desc}>Let the teammate to scan above code:</Text>
-                <QRCode value={activeProject} size={dim.width - 40} bgColor="rgb(0,0,0)" fgColor="white" />
+                <QRCode value={`joinToProject:${activeProject}`} size={dim.width - 40} bgColor="rgb(0,0,0)" fgColor="white" />
             </View>
         );
     }
@@ -22,13 +21,13 @@ const ShareScreen: React.FC = () => {
 
 const styles = StyleSheet.create({
     desc: {
-        marginBottom: 20
+        marginBottom: 20,
     },
     qrWrapper: {
         flex: 1,
         alignItems: "center",
-        justifyContent: "center"
-    }
-})
+        justifyContent: "center",
+    },
+});
 
 export default ShareScreen;
