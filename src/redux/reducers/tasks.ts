86f81f1708ce1d9f4f3c0228e3093fb3039dc79d
Jakub Mandra <mandra.jakub@gmail.com>
2020-02-23 19:41:24 +0100

    Map state in redux actions

--- src/redux/reducers/tasks.ts
+++ src/redux/reducers/tasks.ts
@@ -4,10 +4,12 @@ export const tasks = (state = initialState, action) => {
     switch (action.type) {
         case "TASKS_FETCH_SUCCESS":
             return {
+                ...state,
                 columns: [...action.columns],
             }
         case "UPDATE_TASKS_LIST":
             return {
+                ...state,
                 columns: [...action.columns],
             }
         default:
