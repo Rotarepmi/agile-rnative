59e95494469f7080d00ade5e13a36a45fd762b3f
Jakub Mandra <mandra.jakub@gmail.com>
2020-02-23 19:42:12 +0100

    Clear KanbardBoradContainer code

--- src/screens/kanbanBoard/KanbanBoardContainer.tsx
+++ src/screens/kanbanBoard/KanbanBoardContainer.tsx
@@ -1,14 +1,9 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { db } from "../../utils/firebase";
 import { tasksFetchSuccess } from "../../redux/actions";
 import KanbanBoardView from "./KanbanBoardView";
-import { NavigationSwitchProp, NavigationScreenComponent, NavigationSwitchScreenProps } from "react-navigation";
-
-interface Props {
-    navigation: NavigationSwitchProp;
-}
 
 const KanbanBoardContainer: React.FC = () => {
     const columns = useSelector(state => state.tasks.columns);
@@ -17,57 +12,29 @@ const KanbanBoardContainer: React.FC = () => {
 
     const [loading, setLoading] = useState(true);
 
-    const fetchProject = useCallback(projectId => {
-        setLoading(true);
-
-        // if (!!projectId) {
-        //     db.collection("projects")
-        //         .doc(projectId)
-        //         .collection("tasksLists")
-        //         .orderBy("place")
-        //         .get()
-        //         .then(querySnap => {
-        //             let cols = [];
-
-        //             querySnap.forEach(result => {
-        //                 cols.push({ id: result.id, ...result.data() });
-        //             });
-        //             return cols;
-        //         })
-        //         .then(cols => {
-        //             dispatch(tasksFetchSuccess(cols));
-        //             setLoading(false);
-        //         })
-        //         .catch(e => console.log(e));
-        // }
-
-        if (!!projectId) {
-            db.collection("projects")
-                .doc(projectId)
-                .collection("tasksLists")
-                .orderBy("place")
-                .onSnapshot(querySnapshot => {
-                    let cols = [];
-
-                    querySnapshot.forEach(result => {
-                        cols.push({ id: result.id, ...result.data() });
-                    });
-
-                    dispatch(tasksFetchSuccess(cols));
-                    setLoading(false);
-                });
-        }
-    }, []);
-
     useEffect(() => {
-        fetchProject(activeProject);
+        const unsubscribe = db
+            .collection("projects")
+            .doc(activeProject)
+            .collection("tasksLists")
+            .orderBy("place")
+            .onSnapshot(querySnapshot => {
+                // var source = querySnapshot.metadata.hasPendingWrites ? "Local" : "Server";
+
+                let cols = [];
+
+                querySnapshot.forEach(result => {
+                    cols.push({ id: result.id, ...result.data() });
+                });
+
+                dispatch(tasksFetchSuccess(cols));
+                setLoading(false);
+            });
+
+        return () => unsubscribe();
     }, [activeProject]);
 
     return <KanbanBoardView columns={columns} loading={loading} />;
 };
-
-// KanbanBoardContainer.navigationOptions = ({ navigation }) => ({
-//     title: "Woof",
-// });
 
 export default KanbanBoardContainer;
