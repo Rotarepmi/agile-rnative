c085736035b5e23591a385ad901baa8f95581d7b
Jakub Mandra <mandra.jakub@gmail.com>
2020-03-03 21:01:19 +0100

    Update layout of qr code

--- src/screens/ShareScreen.tsx
+++ src/screens/ShareScreen.tsx
@@ -2,16 +2,26 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { View, Text, StyleSheet, Dimensions } from "react-native";
 import QRCode from "react-native-qrcode";
+import { theme } from "../utils/theme";
+
+const dim = Dimensions.get("screen");
 
 const ShareScreen: React.FC = () => {
     const activeProject = useSelector(state => state.projects.activeProject);
-    const dim = Dimensions.get("screen");
 
     if (dim) {
         return (
-            <View style={styles.qrWrapper}>
-                <Text style={styles.desc}>Let the teammate to scan above code:</Text>
-                <QRCode value={`joinToProject:${activeProject}`} size={dim.width - 40} bgColor="rgb(0,0,0)" fgColor="white" />
+            <View style={styles.qrScreenWrapper}>
+                <Text style={styles.desc}>Let the teammate to scan this code:</Text>
+                <View style={styles.qrWrapper}>
+                    <QRCode
+                        value={`joinToProject:${activeProject}`}
+                        size={dim.width*1.5}
+                        bgColor="rgb(0,0,0)"
+                        fgColor="white"
+                        style={styles.qrCode}
+                    />
+                </View>
             </View>
         );
     }
@@ -23,11 +33,25 @@ const styles = StyleSheet.create({
     desc: {
         marginBottom: 20,
     },
-    qrWrapper: {
+    qrScreenWrapper: {
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
     },
+    qrWrapper: {
+        position: "relative",
+        flex: 0,
+        backgroundColor: theme.white,
+        width: dim.width - 40,
+        height: dim.width - 40,
+        overflow: "hidden"
+    },
+    qrCode: {
+        flex: 0,
+        position: "absolute",
+        top: 50,
+        left: 50
+    }
 });
 
 export default ShareScreen;
